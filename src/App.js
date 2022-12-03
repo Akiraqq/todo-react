9d499a3de717a8662cc14da43fb162bac9deb820
Akira <dzirt1907@rambler.ru>
2022-12-03 23:17:50 +0200

    used localStorage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import './App.css'
 import TodoForm from './components/Todos/TodoForm'
 import TodoList from './components/Todos/TodoList'
 import TodosActions from './components/Todos/TodosActions'
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const localStorageTodosRaw = localStorage.getItem('todos')
+  let localStorageTodos = []
+  localStorageTodosRaw && (localStorageTodos = JSON.parse(localStorageTodosRaw))
+  const [todos, setTodos] = useState(localStorageTodos)
+
+  useMemo(() => {
+    localStorage.clear()
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const createTodo = (newTodo) => {
     setTodos([...todos, newTodo])
+    localStorageTodos
+      ? localStorage.setItem(
+          'todos',
+          JSON.stringify([...localStorageTodos, newTodo])
+        )
+      : localStorage.setItem('todos', JSON.stringify([newTodo]))
   }
+
   const removeTodo = (todo) => {
     setTodos(todos.filter((t) => t.id !== todo.id))
   }
