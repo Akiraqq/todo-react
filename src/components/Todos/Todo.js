d57b429fc843361c0be1c4025f03d1308dfa816a
Akira <dzirt1907@rambler.ru>
2022-12-04 04:39:40 +0200

    Finaly version

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,14 +1,16 @@
 import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri'
 import { FaCheck } from 'react-icons/fa'
 import styles from './Todo.module.css'
+import { useState } from 'react'
 const Todo = ({ todo, remove, toggleTodo }) => {
+  const [active, setActive] = useState(false)
   return (
     <div
       className={`${styles.todo} ${
         todo.isCompleted ? styles.completedTodo : ''
       }`}
     >
-      <RiTodoFill className={styles.todoIcon} />
+      <RiTodoFill className={styles.todoIcon} onClick={() => setActive(true)} />
       <div className={styles.todoText}>{todo.text}</div>
       <RiDeleteBin2Line
         className={styles.deleteIcon}
@@ -18,6 +20,22 @@ const Todo = ({ todo, remove, toggleTodo }) => {
         className={styles.checkIcon}
         onClick={() => toggleTodo(todo.id)}
       />
+      <div
+        onClick={() => setActive(false)}
+        className={
+          active ? `${styles.modal} ${styles.active}` : `${styles.modal}`
+        }
+      >
+        <div
+          className={
+            active
+              ? `${styles.modal__content} ${styles.active}`
+              : `${styles.modal__content}`
+          }
+        >
+          {todo.text}
+        </div>
+      </div>
     </div>
   )
 }
